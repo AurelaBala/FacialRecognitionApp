c76baa36a811e911c279d5ebceee741cbed90b4f
AurelaBala <aurelatosuni2022@gmail.com>
2023-04-02 21:30:16 -0400

    adding the getPhoto() function

--- FacialRecognitionApp/ViewController.swift
+++ FacialRecognitionApp/ViewController.swift
@@ -9,14 +9,30 @@ import UIKit
 import Vision
 import Photos
 
+
+
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var pictureChosen: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getImage(_ sender: UIButton)
+    {
+        getPhoto()
+    }
+    
+    func getPhoto() {
+     let picker = UIImagePickerController()
+     picker.delegate = self
+     picker.sourceType = .savedPhotosAlbum
+     present(picker, animated: true, completion: nil)
+     }
 }
 
